f8ef776a583fdede3c3d159a95475ddd82921884
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-10-09 08:25:29 +0900

    refactor: store as instance variable

--- Source/Assets/Mikazuki/EditorExtensions/Scripts/Reflections/TreeViewState.cs
+++ Source/Assets/Mikazuki/EditorExtensions/Scripts/Reflections/TreeViewState.cs
@@ -5,6 +5,8 @@ namespace Assets.Mikazuki.EditorExtensions.Scripts.Reflections
 {
     public class TreeViewState : ReflectionAccessor<UnityEditor.IMGUI.Controls.TreeViewState>
     {
+        private readonly RenameOverlay _renameOverlay;
+
         public List<int> SelectedIds
         {
             get { return Instance.selectedIDs; }
@@ -15,12 +17,14 @@ namespace Assets.Mikazuki.EditorExtensions.Scripts.Reflections
             get { return Instance.expandedIDs; }
         }
 
-        public TreeViewState(UnityEditor.IMGUI.Controls.TreeViewState instance) : base(instance) { }
+        public TreeViewState(UnityEditor.IMGUI.Controls.TreeViewState instance) : base(instance)
+        {
+            _renameOverlay = new RenameOverlay(AccessProperty<object>("renameOverlay", BindingFlags.Instance | BindingFlags.NonPublic));
+        }
 
         public bool IsRenaming(int instanceId)
         {
-            var renameOverlay = new RenameOverlay(AccessProperty<object>("renameOverlay", BindingFlags.Instance | BindingFlags.NonPublic));
-            return renameOverlay.IsRenaming() && renameOverlay.UserData == instanceId;
+            return _renameOverlay.IsRenaming() && _renameOverlay.UserData == instanceId;
         }
     }
 }
