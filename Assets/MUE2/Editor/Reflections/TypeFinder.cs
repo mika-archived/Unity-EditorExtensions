ae97b7d20b6543309f57317308ed433204e793b8
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-10-20 01:14:31 +0900

    fear: add type finder for Unity

---
+++ Assets/MUE2/Editor/Reflections/TypeFinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MUE2.Editor.Reflections
+{
+    internal static class TypeFinder
+    {
+        public static Type GetType(string fullname)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var t = assembly.GetType(fullname);
+                if (t != null)
+                    return t;
+            }
+
+            return null;
+        }
+    }
+}
