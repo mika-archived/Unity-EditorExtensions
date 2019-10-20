b1836868ac99e54186f3a892f8595f60635e3745
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-10-20 23:27:56 +0900

    feat: cache type

--- Assets/MUE2/Editor/Reflections/TypeFinder.cs
+++ Assets/MUE2/Editor/Reflections/TypeFinder.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MUE2.Editor.Reflections
 {
     internal static class TypeFinder
     {
+        private static readonly Dictionary<string, Type> Caches;
+
+        static TypeFinder()
+        {
+            Caches = new Dictionary<string, Type>();
+        }
+
         public static Type GetType(string fullname)
         {
+            if (Caches.ContainsKey(fullname))
+                return Caches[fullname];
+
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
                 var t = assembly.GetType(fullname);
-                if (t != null)
-                    return t;
+                if (t == null)
+                    continue;
+
+                Caches.Add(fullname, t);
+                return t;
             }
 
             return null;
