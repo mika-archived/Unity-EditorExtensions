3b9bc77bc10b965c59248c9cb2a5d734d16136b0
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-10-22 18:24:06 +0900

    feat: add package exporter

---
+++ Assets/MUE2/Editor/PackageExport.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+
+using UnityEditor;
+
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+#pragma warning disable RCS1110 // Declare type inside namespace.
+
+internal static class PackageExport
+#pragma warning restore RCS1110 // Declare type inside namespace.
+{
+    [MenuItem("Tools/Export as UnityPackage")]
+    public static void Export()
+    {
+        var path = Path.Combine(Application.dataPath, "MUE2");
+        var assets = Directory.GetFiles(path, "*", SearchOption.AllDirectories)
+                              .Where(w => Path.GetExtension(w) == ".cs" && !w.EndsWith("PackageExport.cs"))
+                              .Select(w => "Assets" + w.Replace(Application.dataPath, "").Replace("\\", "/"))
+                              .ToArray();
+
+        Debug.Log(string.Format("Export: {0}", string.Join(", ", assets)));
+
+        AssetDatabase.ExportPackage(assets, "./MUE2.unitypackage", ExportPackageOptions.Default);
+    }
+}
